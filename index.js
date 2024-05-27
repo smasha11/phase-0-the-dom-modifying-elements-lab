44f728f55888699a85cd5a635f41af8bef9f0f80
smasha11 <ismail.hussein.1120@gmail.com>
2024-05-27 11:55:40 +0300

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const mainelement = document.getElementById('main')
+mainelement.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'ISMAIL is the champion';
+document.body.appendChild(newHeader);
